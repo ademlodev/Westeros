34edf6e079d80f4bfb037f21a0199e4be875162c
Javier Garcia <javier.garcia.olmeda@gmail.com>
2018-02-08 21:42:48 +0100

    Initialize the first window

--- Westeros/AppDelegate.swift
+++ Westeros/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.cyan // se podria poner solo .cyan
+        
+        // la hacemos visible al no ser storyboard
+        window?.makeKeyAndVisible()
+        
+        let root = UIViewController()
+        window?.rootViewController = root
+        
         return true
     }
 
