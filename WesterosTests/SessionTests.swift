a1729557f870d1be61d5d7947b13b98fe4e0c5e6
Javier Garcia <javier.garcia.olmeda@gmail.com>
2018-02-25 11:54:58 +0100

    Clase de test para sesiones

---
+++ WesterosTests/SessionTests.swift
@@ -0,0 +1,14 @@
+//
+//  SessionTests.swift
+//  WesterosTests
+//
+//  Created by Javi on 24/2/18.
+//  Copyright © 2018 Javi. All rights reserved.
+//
+
+import XCTest
+
+@testable import Westeros
+class SessionTests: XCTestCase {
+    
+}
